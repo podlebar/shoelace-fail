195e0fa49a00ef7b8ee7a08b3cf91c7264f7d2c2
Bartosz Podlewski <b.podlewski@gmail.com>
2023-08-06 15:22:31 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <h1>QR code generator</h1>
+
+    <sl-input maxlength="255" clearable label="Value" v-model="qrCode"></sl-input>
+
+    <sl-qr-code :value="qrCode"></sl-qr-code>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+  import '@shoelace-style/shoelace/dist/components/qr-code/qr-code.js';
+  import '@shoelace-style/shoelace/dist/components/input/input.js';
+
+  const qrCode = ref();
+</script>
+
+<style>
+  .container {
+    max-width: 400px;
+    margin: 0 auto;
+  }
+
+  sl-input {
+    margin: var(--sl-spacing-large) 0;
+  }
+</style>
